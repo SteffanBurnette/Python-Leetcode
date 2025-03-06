10a0bbda2c74e228bb62e0f55d57fc735eeae3f4
Steffan Burnette <burnettesteffansep7@gmail.com>
2025-03-06 16:42:53 -0500

    group anagrams

---
+++ HashMaps/groupAnagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = []
+        my_hash = {}
+       
+        
+        for i in range(len(strs)):
+            temp_ana = []
+            print("".join(sorted(strs[i])))
+            for j in range(len(strs)):
+                if sorted(strs[i]) == sorted(strs[j]) and "".join(sorted(strs[i])) not in my_hash:
+                    temp_ana.append(strs[j])
+            if len(temp_ana) >= 1:
+                anagrams.append(temp_ana)
+            my_hash["".join(sorted(strs[i]))] = True
+           
+
+
+        return anagrams
